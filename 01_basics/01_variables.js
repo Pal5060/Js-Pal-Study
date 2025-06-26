b5f6a26426d55cf8cc3cbcae1f1b3cba504647ca
Pal Prajapati <palprajapati5060@gmail.com>
2025-06-26 08:25:40 +0000

    Add variables for account details and log output

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail= "pal@gmail.com"
+var accountPassword = "12345"
+accountCity = "ahemadabad"
+let accountState = "gujarat"
+
+
+// accountId = 2
+accountEmail = "ps@ps.com"
+accountPassword = "1234"
+accountCity = "surat"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
