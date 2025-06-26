ea55716a4d28efa23a729de0813e391b8cde0350
Pal Prajapati <palprajapati5060@gmail.com>
2025-06-26 09:15:28 +0000

    Datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,12 +4,15 @@ var accountPassword = "12345"
 accountCity = "ahemadabad"
 let accountState = "gujarat"
 
-
 // accountId = 2
 accountEmail = "ps@ps.com"
 accountPassword = "1234"
 accountCity = "surat"
 
+/*
+Prefer not using var
+becuse of issue in block scope and function scope
+*/
 
 console.log(accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all javascript code as newer version
+
+// alert (3  + 4); // 7 in browser
+
+console.log(3 + 4); // 7 in console
+
+console.log("Pal"); // Pal in console
+
+let name = "Pal"; // string literal
+let age = 21; // number literal
+let isLoggedIn = false; // boolean literal
+
+// number => 2 to power 53
+// BigInt => 2 to power 53 and above
+// string => "Pal"
+// boolean => true or false
+// null => stands alone value
+// undefined => variable not defined
+// Symbol => unique and immutable value
+
+
+
+// objects
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
