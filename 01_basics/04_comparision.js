54a869a2e4b36e902871cda68d51c348e59bc90b
Pal Prajapati <palprajapati5060@gmail.com>
2025-06-28 05:51:07 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 <= 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+
+
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+
+// that all comparison are avoid make clear code 
+// console.log(null > 0); // false
+// console.log(null >= 0); // true
+// console.log(null == 0); // false
+// console.log(null == undefined); // true
+// console.log(undefined > 0); // false
+
+// === sccrict equality
+// console.log(2 === 2); // true
+// console.log(2 === "2"); // false
+// console.log(2 !== 2); // false
+// console.log(2 !== "2"); // true
