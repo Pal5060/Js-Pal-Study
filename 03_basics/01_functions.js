1d584a5d3a30311ad7c85b37038ade6c7d64719b
Pal5060 <palprajapati5060@gmail.com>
2025-07-28 09:06:17 +0530

    Add functions for name logging and user login message handling in JavaScript

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,36 @@
+ 
+function sayMyName(){
+    console.log("p");
+    console.log("a");
+    console.log("l");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1 , number2) {
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1 , number2) {
+    // let result = number1 + number2;
+    // return result;
+    return number1 + number2;
+}
+
+const result = addTwoNumbers(5, 10)
+
+// console.log("Result:", result);
+
+
+function loginUserMessage (username = "Guest") 
+{
+    if (! username  ){
+        console.log("Please provide a username");
+        return 
+    }
+    return`${username} logged in successfully`
+}
+
+// console.log(loginUserMessage("pal"))
+console.log(loginUserMessage())
+
