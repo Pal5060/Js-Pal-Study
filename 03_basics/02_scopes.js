5e101c918a145e967126ec900b405c1403604000
Pal5060 <palprajapati5060@gmail.com>
2025-07-29 10:45:06 +0530

    Add initial implementation of variable scope demonstration in JavaScript

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+// var c=300
+ let a = 300
+if (true) {
+
+    let a = 10 
+    const b= 20
+    console.log("INNER:",a);
+
+}
+
+for (let index = 0; index < 5; index++) {
+   const element = array[index];
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
