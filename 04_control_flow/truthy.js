81daeddcc7bffec73b38195969e919b0965bd288
Pal5060 <palprajapati5060@gmail.com>
2025-08-05 12:10:37 +0530

    Add truthy and falsy values demonstration with user email and object checks

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,45 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+    
+}else {
+    console.log("No user email");
+}
+
+
+// falsy values
+
+// false 0 , -0 , BigInt 0n, "",   null, undefined, NaN
+
+// truthy values
+
+// true, 1, -1, BigInt 1n, " ", "0",'false', [], {}, function() {}, new Date(), Infinity, -Infinity
+
+// if (userEmail.length === 0 ) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj  = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+    
+}
+
+// nullish coalescing operator (??): null or undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = val1 ?? 10 //10
+// val1 = null ?? 10 //10
+// val1 = undefined ?? 10; // 10
+val1 = null ?? 10 ?? 20
+console.log(val1); // 5
+
+// Ternary operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("Ice tea is affordable") : console.log("Ice tea is expensive");
